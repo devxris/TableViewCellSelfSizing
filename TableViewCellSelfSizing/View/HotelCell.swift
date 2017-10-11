e3bd3a102705bb3e51cb70d1ba596c542c3a93cc
devxris <chuwei0915@gmail.com>
2017-10-11 09:46:18 +0800

    Simple UI and Setup

---
+++ TableViewCellSelfSizing/View/HotelCell.swift
@@ -0,0 +1,16 @@
+//
+//  HotelCell.swift
+//  TableViewCellSelfSizing
+//
+//  Created by Chris Huang on 11/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class HotelCell: UITableViewCell {
+
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var addressLabel: UILabel!
+	@IBOutlet weak var descriptionLabel: UILabel!
+}
